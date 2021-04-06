50587b9aa47d7a190a5d8ca0c69907f99df57d22
Måns Anderssone <mans.andersson97@gmail.com>
2021-04-06 14:37:30 +0200

    Add comments

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const key_word_dict_string = core.getInput("keyword-dict", { required: true })
-const label_dict = JSON.parse(key_word_dict_string);
+const label_dict = JSON.parse(key_word_dict_string); // a dictionary containing the permitted labels and their corresponding keywords/ keyphrases
 
 async function run() {
+  // this is the main function run by the label-generating action
   try {
-  const token = core.getInput("repo-token", { required: true });
+  const token = core.getInput("repo-token", { required: true }); // the token used for authentication
   const client = new github.getOctokit(token);
-  const prNr = github.context.payload.number;
-  const prTitle = github.context.payload.pull_request.title;
-  await addLabels(client, prNr, prTitle);
+  const prNr = github.context.payload.number; // the issue-number of the PR
+  const prTitle = github.context.payload.pull_request.title; // the title of the PR
+  const labels = genLabels(prTitle); // generates the labels from the title of  the PR
+  await addLabels(client, prNr, prTitle, labels); // adds the generated labels to the PR
   } catch (error) {
   core.setFailed(error.message);
   console.log(error);
   }
 }
 
-async function addLabels(client, prNumber, prTitle) {
-  const labels = genLabels(prTitle);
+async function addLabels(client, prNumber, prTitle, labels) {
+  // this function adds labels to a PR
   if(labels.length === 0) {
-    console.log("No matchins labels found, exiting");
+    console.log("No matching labels found, exiting"); // no keywords/keyphrases were found in the title of the PR
     return;
   }
   console.log("Found matching labels:", labels);
@@ -32,6 +34,7 @@ async function addLabels(client, prNumber, prTitle) {
 }
 
 function genLabels(prTitle){
+  // this function generates labels from keywords found in PR-title
   prTitle = prTitle.toLowerCase();
   var labels = []; // labels to be added to PR
   for(const keyword in label_dict) {
